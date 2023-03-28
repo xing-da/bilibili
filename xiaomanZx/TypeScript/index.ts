93a729742e7c3abefa498158139f4b2d88f3f75d
342169159@qq.com <342169159@qq.com>
2023-03-28 14:58:15 +0800

    函数类型

--- xiaomanZx/TypeScript/index.ts
+++ xiaomanZx/TypeScript/index.ts
@@ -128,3 +128,62 @@
 //     length:number
 //     [index:number]:any
 // }
+
+//函数类型
+// 1.函数定义类型和返回值 | 箭头函数定义类型和返回值
+// 2.函数默认的参数 | 函数可选参数 ?
+// 3.参数是一个对象如何定义
+// 4.函数this类型
+// 5.函数重载 
+// function add(a: number, b: number): number {
+//     return a + b
+// }
+// const add = (a: number, b: number): number => a + b
+// console.log(add(2, 4));
+//.
+// function add(a: number, b: number = 5): number {
+//     return a + b
+// }
+// console.log(add(1));
+// .
+// interface User {
+//     name: string
+//     age: number
+// }
+// function add(user: User): User {
+//     return user
+// }
+// console.log(add({ name: '代', age: 4 }));
+// .
+// interface Obj {
+//     user: number[]
+//     add: (this: Obj, num: number) => void
+// }
+// // ts 可以定义this的类型 在js中无法使用 必须是第一个参数定义this 的类型
+// let obj: Obj = {
+//     user: [1, 2, 3],
+//     add(this: Obj, num: number) {
+//         this.user.push(num)
+//     }
+// }
+// obj.add(4)
+// console.log(obj.user);
+// .
+// let user: number[] = [1, 2, 3];
+// function findNum(): number[]//如果没有传入东西就是查询全部
+// function findNum(id: number): number[]//如果传入了id就单个查询
+// function findNum(add: number[]): number[]//如果传入的是一个number类型的数组那就做添加
+// function findNum(ids?: number | number[]): number[] {
+//     if (typeof ids == 'number') {
+//         return user.filter(v => v == ids)
+//     }
+//     else if (Array.isArray(ids)) {
+//         user.push(...ids)
+//         return user
+//     } else {
+//         return user
+//     }
+// }
+// console.log(findNum());
+// console.log(findNum(3));
+// console.log(findNum([4]));
