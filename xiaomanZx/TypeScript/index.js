0e0b8b68c43e2b6a5c39583a23b78e984dc7f339
342169159@qq.com <342169159@qq.com>
2023-03-28 11:19:57 +0800

    tsBasics

--- xiaomanZx/TypeScript/index.js
+++ xiaomanZx/TypeScript/index.js
@@ -1,27 +1,22 @@
+"use strict";
 // let str:String = '代';
 // console.log(str);
-
 // let num:number = 123;
 // let s:String = `${num}`;
 // console.log(s);
-
 // /* Type Checking */
 // "strict": false, 
 // let v1:void = null
 // let v2:void = undefined
-
 // 可以穿插赋值
 //需要把严格模式关闭
 // let n:null = null;
 // let a:undefined = undefined;
 // n = a
 // a = n
-
 // void应用场景
 // function myFn():void {
-
 // }
-
 function myFn() {
-    return 1
+    return 1;
 }
