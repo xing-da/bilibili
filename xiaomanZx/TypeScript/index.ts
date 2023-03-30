33183ab60aeac6874c4a96a85c80aa4ad487318d
342169159@qq.com <342169159@qq.com>
2023-03-30 09:02:25 +0800

    Class

--- xiaomanZx/TypeScript/index.ts
+++ xiaomanZx/TypeScript/index.ts
@@ -281,69 +281,99 @@
 // 3.super 原理
 // 4.静态方法
 // 5.get set
-interface Options {
-    el: string | HTMLElement
-}
-interface VueCls {
-    options: Options
-    init(): void
-}
+// interface Options {
+//     el: string | HTMLElement
+// }
+// interface VueCls {
+//     options: Options
+//     init(): void
+// }
 
-interface Vnode {
-    tag: string
-    text?: string
-    children?: Vnode[]
-}
-// 虚拟dom 简单版
-class Dom {
-    //创建节点的方法
-    createElement(el: string) {
-        return document.createElement(el)
-    }
-    // 填充文本的方法
-    setText(el: HTMLElement, text: string | null) {
-        el.textContent = text;
-    }
-    // 渲染函数
-    render(data: Vnode) {
-        let root = this.createElement(data.tag)
-        if (data.children && Array.isArray(data.children)) {
-            data.children.forEach(item => {
-                let child = this.render(item)
-                root.appendChild(child)
-            })
-        } else {
-            this.setText(root, data.text)
-        }
-        return root;
-    }
-}
-class Vue extends Dom implements VueCls {
-    options: Options
-    constructor(options: Options) {
-        super()
-        this.options = options
-        this.init()
-    }
-    init(): void {
-        let data: Vnode = {
-            tag: "div",
-            children: [
-                {
-                    tag: 'section',
-                    text: '我是子节点1'
-                },
-                {
-                    tag: 'section',
-                    text: '我是子节点2'
-                },
-            ]
-        }
-        let app = typeof this.options.el == 'string' ? document.querySelector(this.options.el) : this.options.el
-        app.appendChild(this.render(data))
+// interface Vnode {
+//     tag: string
+//     text?: string
+//     children?: Vnode[]
+// }
+// // 虚拟dom 简单版
+// class Dom {
+//     constructor(x) {
+//         console.log(x);
 
-    }
-}
-new Vue({
-    el: "#app",
-})
+//     }
+//     //创建节点的方法
+//     createElement(el: string) {
+//         return document.createElement(el)
+//     }
+//     // 填充文本的方法
+//     setText(el: HTMLElement, text: string | null) {
+//         el.textContent = text;
+//     }
+//     // 渲染函数
+//     render(data: Vnode) {
+//         let root = this.createElement(data.tag)
+//         if (data.children && Array.isArray(data.children)) {
+//             data.children.forEach(item => {
+//                 let child = this.render(item)
+//                 root.appendChild(child)
+//             })
+//         } else {
+//             this.setText(root, data.text)
+//         }
+//         return root;
+//     }
+// }
+// class Vue extends Dom implements VueCls {
+//     options: Options
+//     constructor(options: Options) {
+//         //父类的prototype.constructor.call
+//         super('代')
+//         this.options = options
+//         this.init()
+//     }
+//     // 静态方法 this能互调跟属性,但无法调用非静态方法
+//     static version() {
+//         return '1.0.0'
+//     }
+//     init(): void {
+//         let data: Vnode = {
+//             tag: "div",
+//             children: [
+//                 {
+//                     tag: 'section',
+//                     text: '我是子节点1'
+//                 },
+//                 {
+//                     tag: 'section',
+//                     text: '我是子节点2'
+//                 },
+//             ]
+//         }
+//         let app = typeof this.options.el == 'string' ? document.querySelector(this.options.el) : this.options.el
+//         app.appendChild(this.render(data))
+
+//     }
+// }
+// new Vue({
+//     el: "#app",
+// })
+// Vue.version()
+
+// .
+// class Ref {
+//     _value: any
+//     constructor(value: any) {
+//         this._value = value
+//     }
+
+//     get value() {
+//         return this._value + 'ddd'
+//     }
+//     set value(newValue) {
+//         this._value = newValue + 'd'
+//     }
+// }
+
+// const ref = new Ref('哈哈哈')
+// console.log(ref.value);
+// ref.value = 'tt'
+// console.log(ref.value);
